fa4c1a531bbb04634a7c5d06c38c93dc7f4b344a
bloody-slipper <anniershova@gmail.com>
2022-09-04 22:44:38 +0300

    added tests in junit4

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,40 @@
+package ru.netology.service;
+
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CashbackHackServiceTest {
+
+    CashbackHackService service = new CashbackHackService();
+
+    @Test
+
+    public void shouldCashbackIfAmountLessBoundary() {
+
+        int expected = 400;
+        int actual = service.remain(600);
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+
+    public void shouldCashbackIfAmountMoreBoundary() {
+
+        int expected = 200;
+        int actual = service.remain(2800);
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+
+    public void shouldCashbackIfAmountEqualsBoundary() {
+
+        int expected = 0;
+        int actual = service.remain(1000);
+
+        Assert.assertEquals(expected, actual);
+    }
+}
